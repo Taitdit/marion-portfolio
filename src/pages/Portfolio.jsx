4dbe74938166be6616b9f296cb7eedd7a92e18ff
DEMUYTER <tdemuyter@seolis.net>
2026-08-03 18:02:33 +0200

    feat(): page portfolio LOT01

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,15 @@
+import Bloctitle from '../components/Bloctitle'
+import Grid from "../components/Grid"
+
 const Portfolio = () => {
+    const introContenu = () => <>Découvrez une selection de mes différentes réalisations.<br/>Chaque projet est une nouvelle quete, un nouveau défi que j’aime relever.</>
     return (
-        <div>Portfolio</div>
+        <>
+        <Bloctitle title="Portfolio" intro={introContenu()} />
+        <div className='portfolio__container'>
+            <Grid />
+        </div>
+        </>
     )
 }
 export default Portfolio
