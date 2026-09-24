0dfada253699e21c612fb1176267a88e7e24147a
Marion <charbonnier.marion@outlook.fr>
2026-09-24 19:51:43 +0200

    modifs visuels icones et autre imamge

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -24,7 +24,7 @@ const Info = () => {
         {
             img: 'disponibilite',
             title: 'Disponibilité',
-            detail: 'Sous 2 mois'
+            detail: 'Sous 1 mois'
         }
     ]
 
